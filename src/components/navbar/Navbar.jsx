61df233e5bcffb90e7c10c54c3d4bb94168002f4
witthayadevkub <tigermixdev@gmail.com>
2024-01-19 16:29:58 +0700

    finished part of section box story show

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import './Navbar.css'
 
 //react-icon
@@ -8,7 +9,25 @@ import { IoNotificationsOutline } from "react-icons/io5";
 
 import { TiHome } from "react-icons/ti";
 import { FaBars, FaSearch, FaUserFriends} from "react-icons/fa";
+
+import { useState } from 'react'
+
+
 const Navbar = () => {
+
+    const [alart, setAlart] = useState({
+
+        chat:1,
+        friends:0,
+        video:9,
+        notification:10,
+        community:0,
+
+    })
+
+
+    // setAlartChat(1)
+
   return (
     <div>
         <nav className='layout'>
@@ -38,30 +57,44 @@ const Navbar = () => {
                     <p></p>
                 </div>
 
-                <div className="friend">
-                    <i >  <FaUserFriends /> </i>
-                   
+                {alart.friends !== 0 ? 
+                <div className="box-icon">
+                   <i >  <FaUserFriends /> </i>
+                    <p>{alart.friends}</p>
                 </div>
+                : <i >  <FaUserFriends /> </i>}
+                
 
+                {alart.chat !== 0 ? 
                 <div className="box-icon">
                     <i><RiMessengerLine/></i>
-                    <p>1</p>
-                    </div>
-
+                    <p>{alart.chat}</p>
+                </div> 
+                : <i><RiMessengerLine/></i>}
+                
+                {alart.video !== 0 ? 
                 <div className="box-icon">
                     <i><MdOndemandVideo/></i>
-                    <p>9</p>
+                    <p>{alart.video}</p>
                 </div>
+                : <i><MdOndemandVideo/></i>}
+                
 
+                {alart.notification !== 0 ? 
                 <div className="box-icon">
                     <i><IoNotificationsOutline/></i>
-                    <p>10</p>
+                    <p>{alart.notification}</p>
                 </div>
+                : <i><IoNotificationsOutline/></i>}
+                
 
-                <div className="community">
+                {alart.community != 0 ? 
+                <div className="box-icon">
                     <i class="fa-solid fa-users-rectangle" ></i>
-                     
-                </div>
+                     <p>{alart.community}</p>
+                </div> 
+                : <i class="fa-solid fa-users-rectangle" ></i>}
+                
             </div>          
         </nav>
 
