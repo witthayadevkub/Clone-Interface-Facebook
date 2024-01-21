60cd3d4c453c0b7d9efaa53f14a616db3ec146d8
WitthayaFaengsap <124575181+witthayadevkub@users.noreply.github.com>
2024-01-21 12:17:35 +0700

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             <div className="topNav">
                     <div className="logo">
                             {/* <h1>facebook</h1> */}
-                            <img src="https://images.unsplash.com/photo-1636123313721-d5587afbb084?q=80&w=1176&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'" alt="" />
+                            <img src="https://logos-world.net/wp-content/uploads/2020/04/Facebook-Logo.png'" alt="" />
                     </div>
 
                         <div className="nav-topleft">
